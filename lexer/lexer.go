07b87e7e93af8c07985a612267342efc0e45237c
Juan Bustos <juan.bustos@wnco.com>
2025-02-17 22:08:13 -0600

    monkey - added EQ and NOT EQ to lexer. Updated unit tests

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -48,8 +48,17 @@ func (l *Lexer) NextToken() token.Token {
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
+
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -73,7 +82,16 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RBRACE, l.ch)
 
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
@@ -96,6 +114,14 @@ func (l *Lexer) NextToken() token.Token {
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
